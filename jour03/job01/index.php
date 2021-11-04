30600b2c2837ad38e6dbf13ac2945d292673d9a4
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2021-11-04 15:48:02 +0100

    designer nombre impair ou pair dans un array job 1 jour 3

---
+++ jour03/job01/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$tableau = [200, 204, 173, 98, 171, 404, 459];
+
+foreach ($tableau as $i) {
+    if ($i % 2 == 0) {
+    echo $i. "Est pair". "</br>";
+    }
+    else {
+    echo $i."Est impair"."</br>";
+    }
+
+
+}
+
+
+
+
+?>
