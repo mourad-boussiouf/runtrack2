1ae657bca8757007550a63df5a6fa4c7786c6f8a
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2021-11-04 17:08:44 +0100

    exercice afficher 1 character sur 2 dans la string job 3

---
+++ jour03/job02/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+
+
+for ($i=0; $i<=77 ; $i++ ){
+
+    if ($i % 2 == 0){
+    echo $str[$i];
+    }
+ }
+
+
+
+
+
+
+
+
+?>
