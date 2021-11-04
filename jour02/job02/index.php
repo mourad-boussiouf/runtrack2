c498ef0c550c49ae7c9a966baf90e6ae3acd7fef
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2021-11-04 10:56:37 +0100

    job 2 jour 2

---
+++ jour02/job02/index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+for ($a=0; $a<=1337 ; $a++ ){
+
+    if($a == 26){
+        echo "<br>";
+    }
+
+    elseif ($a == 37) {
+    echo "<br>";
+    
+    }
+    elseif ($a == 88) {
+    echo "<br>";
+    
+    }
+    elseif ($a == 1111) {
+        echo "<br>";
+        
+    }
+    elseif ($a == 3233) {
+            echo "<br>";
+            
+    }
+    
+  else {
+  echo $a."<br>";
+       
+    }
+}
+
+?>
