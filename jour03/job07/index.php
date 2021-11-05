60e0a7e89ff769a724d1230ba3c57bd25e3e9cae
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2021-11-05 17:54:31 +0100

    fin job 07 izip eazy

---
+++ jour03/job07/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$str="Certaines choses changent, et d'autres
+ne changeront jamais.";
+
+for ($b=1;isset($str[$b]); $b++){  //fais commencer la string par le deuxième caractère, puis le troisième est donc le deuxieme etc...
+    echo $str[$b];
+}
+echo $str[0]; //place le premier index à la fin
+?>
