9fe54b403504bf1219d25f791bb8bb8e0797fc2d
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2021-11-07 18:22:21 +0100

    ajout formulaire method GET et post job 01,02,03,04

---
+++ jour04/job04/index.php
@@ -0,0 +1,43 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST" action = "index.php">
+
+<p>
+    <label for = "prenom"> Entrez le prénom : </label>
+    <input type = "text" name = "prenom" id = "prenom" />
+    <label for = "nom"> Entrez le nom : </label>
+    <input type = "text" name = "nom" id = "nom" />
+
+</p>
+
+<p>
+    <input type = "submit" value = Submit />
+
+</p>
+
+</form>
+
+<table>
+<tr>
+<th>Argument : </th>
+<th>Prenom</th>
+<th>Nom</th>
+</tr>
+<tr>
+<td>Valeur : </td>
+<td><?php echo $_POST['prenom'] ?> </td>
+<td><?php echo $_POST['nom'] ?> </td>
+</table>
+
+
